ba434268409565a3d78de29105bc0b8e8b441258
zwo1in <zwolin13@gmail.com>
2020-03-02 19:33:02 +0100

    Close to 3.0

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set>
 
 struct Task;
 using Tasks = std::vector<Task>;
 
 int cmax(Tasks);
+void algorithm(Tasks&);
 
 int main() {
     std::ifstream data;
@@ -25,12 +27,16 @@ int main() {
         if (tmp.find("data.") != std::string::npos) {
             data >> size;
             tasks_list[current].reserve(size);
-            while (size --) {
-                tasks_list[current].emplace_back(data);
+            for (int i = 0; i<size; ++i) {
+                tasks_list[current].emplace_back(i, data);
             }
             current ++;
         }
     } while (current != 4);
+
+    for (auto& tasks: tasks_list) {
+        algorithm(tasks);
+    }
 
     int sum{};
     for (auto& tasks: tasks_list) {
@@ -42,25 +48,65 @@ int main() {
 }
 
 struct Task {
-    int R, P, Q;
+    int id, R, P, Q;
 
-    Task(int r, int p, int q) {
-        R = r;
-        P = p;
-        Q = q;
-    }
+    Task(): id(0), R(0), P(0), Q(0) {}
 
-    Task(std::ifstream& input) {
+    Task(int id, std::ifstream& input): id(id) {
         input >> R >> P >> Q;
     }
+
+    bool operator==(const Task& other) {
+        return this->id == other.id;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Task& t);
 };
+
+std::ostream& operator<<(std::ostream& os, const Task& t) {
+    os << "Task { id: " << t.id << " R: " << t.R << " P: " << t.P << " Q: " << t.Q << " }";
+    return os;
+}
+
 
 int cmax(Tasks tasks) {
     int m{}, c{};
 
-    for (auto task: tasks) {
+    for (const auto& task: tasks) {
         m = std::max(m, task.R) + task.P;
         c = std::max(c, m + task.Q);
     }
     return c;
+}
+
+void algorithm(Tasks& tasks) {
+    std::for_each(tasks.begin(), tasks.end(), [](auto it) { std::cout << it << std::endl; });
+    std::cout << std::endl << std::endl;
+
+    Tasks shortest_R(tasks.size()), shortest_Q(tasks.size()), ret;
+
+    std::partial_sort_copy(tasks.begin(), tasks.end(), shortest_R.begin(), shortest_R.end(), [](const auto& t1, const auto& t2){
+        return t1.R < t2.R;
+    });
+
+    std::partial_sort_copy(tasks.begin(), tasks.end(), shortest_Q.begin(), shortest_Q.end(), [](const auto& t1, const auto& t2){
+        return t1.Q < t2.Q;
+    });
+
+    auto R_begin = shortest_R.begin();
+    auto Q_begin = shortest_Q.rbegin();
+    auto R_end   = shortest_R.end();
+    while (R_begin != R_end) {
+        auto insert = [&](auto iter) {
+            if (std::find(ret.begin(), ret.end(), *iter) == ret.end()) {
+                ret.push_back(*iter);
+            }
+        };
+        insert(R_begin++);
+        insert(Q_begin++);
+    }
+    tasks = ret;
+    std::for_each(tasks.begin(), tasks.end(), [](auto it) { std::cout << it << std::endl; });
+    std::cout << std::endl << std::endl;
+    std::cout << std::endl << std::endl;
 }
